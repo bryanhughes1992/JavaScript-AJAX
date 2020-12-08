f075e9e6497d325fd114c809b1433cd529cbf840
bryannhughes <bryanhughes@hughesbuildingcompany.net>
2020-12-07 21:49:42 -0500

    Rough Copy - Pre-comments

--- contents/assign5-ajax.js
+++ contents/assign5-ajax.js
@@ -1,5 +1,4 @@
 //#### ASSIGNMENT 5 - STORE HOURS ####
-
 window.onload = function() {
     //Store the 'Old City Hall' Radio button as a variable
     var oldCityHall = document.forms.siteHours[0];
@@ -8,28 +7,69 @@ window.onload = function() {
     //Store the 'Union Station' Radio button as a variable
     var unionStation = document.forms.siteHours[2];
 
-    function initAll() {
-        //Store the 'Schedule' text area as a variable
-        var scheduleTable = document.getElementById('schedTbl');
+    //Store the 'Schedule' text area as a variable
+    var scheduleTable = document.getElementById('schedTbl');
 
-        oldCityHall.onclick = getTxtFile;
-
-        function getTxtFile() {
-            var xhr = new XMLHttpRequest();
-
-            xhr.onreadystatechange = function (){
-                if(xhr.readyState === 4){
-                    if(xhr.status === 200) {
-                        scheduleTable.innerHTML = xhr.responseText;
-                    } else {
-                        alert("Connection was unsuccessful");
-                    }
-                }
-            };
-
-            xhr.open("GET", "sched1.txt", true);
-
-            xhr.send(null);
+    oldCityHall.onchange = function () {
+        if (!oldCityHall.checked) {
+            return;
         }
-    }
+        var xhr = new XMLHttpRequest();
+
+        xhr.onreadystatechange = function (){
+            if(xhr.readyState === 4){
+                if(xhr.status === 200) {
+                    scheduleTable.innerHTML = xhr.responseText;
+                } else {
+                    alert("Connection was unsuccessful");
+                }
+            }
+        };
+
+        xhr.open("GET", `sched${oldCityHall.value}.txt`, true);
+
+        xhr.send(null);
+    };
+
+    yongeStreet.onchange = function () {
+        if (!yongeStreet.checked) {
+            return;
+        }
+        var xhr = new XMLHttpRequest();
+
+        xhr.onreadystatechange = function (){
+            if(xhr.readyState === 4){
+                if(xhr.status === 200) {
+                    scheduleTable.innerHTML = xhr.responseText;
+                } else {
+                    alert("Connection was unsuccessful");
+                }
+            }
+        };
+
+        xhr.open("GET", `sched${yongeStreet.value}.txt`, true);
+
+        xhr.send(null);
+    };
+
+    unionStation.onchange = function () {
+        if (!unionStation.checked) {
+            return;
+        }
+        var xhr = new XMLHttpRequest();
+
+        xhr.onreadystatechange = function (){
+            if(xhr.readyState === 4){
+                if(xhr.status === 200) {
+                    scheduleTable.innerHTML = xhr.responseText;
+                } else {
+                    alert("Connection was unsuccessful");
+                }
+            }
+        };
+
+        xhr.open("GET", `sched${unionStation.value}.txt`, true);
+
+        xhr.send(null);
+    };
 };
